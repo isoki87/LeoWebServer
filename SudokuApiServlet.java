e5132c11c225b4a09bb0a638885f13930c0095d5
isoki87 <helloleolin@gmail.com>
2018-05-18 18:46:26 -0700

    Update SudokuApiServlet.java

--- SudokuApiServlet.java
+++ SudokuApiServlet.java
@@ -1,30 +1,28 @@
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.leo.SudokuBoard;
-import com.leo.SudokuSolver;
+import com.leo.sudoku.SudokuBoard;
+import com.leo.sudoku.SudokuGenerator;
 
+
+@SuppressWarnings("serial")
 public class SudokuApiServlet extends HttpServlet {
 	
 	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		String diff = URLDecoder.decode(req.getParameter("difficulty"), "UTF-8");
-		int[][] b
-		if ((word != null) && (word.length() != 0)) {
-			RhymeProvider rp = new RhymeProvider("WordBank.txt");
-			rp.initialize();
-			list = rp.findRhyme(word);
-		}
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		SudokuBoard board = new SudokuBoard();
+		SudokuGenerator sudGen = new SudokuGenerator();
+		sudGen.populateBoard(board);
 		resp.setContentType("text/plain");
 		resp.setCharacterEncoding("UTF-8");
-		for(int i = 0; i < list.size(); i++) {
-			resp.getWriter().println(list.get(i) + "<br>");
+		for(int i = 0; i < 9; i++){
+			for(int j = 0; j < 9; j++){
+				resp.getWriter().print(board.check(i, j));
+			}
 		}
 	}
 }
