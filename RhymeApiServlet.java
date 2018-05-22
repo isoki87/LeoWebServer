033a25b2069320cf595bcf327144122cb16a1e6b
isoki87 <helloleolin@gmail.com>
2018-05-22 11:46:30 -0700

    Switch from embedded jetty to tomcat web container

--- RhymeApiServlet.java
+++ RhymeApiServlet.java
@@ -1,31 +1,39 @@
+package com.leo.servlet;
+
+import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.leo.RhymeProvider;
+import com.leo.rhyme.RhymeProvider;
 
-
-@SuppressWarnings("serial")
+/**
+ * Servlet implementation class RhymeApiServlet
+ */
+@WebServlet("/rhymeapi")
 public class RhymeApiServlet extends HttpServlet {
-	
-	@Override
-	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		String word = URLDecoder.decode(req.getParameter("word"), "UTF-8");
+	private static final long serialVersionUID = 1L;
+  
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String word = URLDecoder.decode(request.getParameter("word"), "UTF-8");
+	    System.out.println(new File(".").getAbsoluteFile());
 		ArrayList<String> list = new ArrayList<>();
 		if ((word != null) && (word.length() != 0)) {
-			RhymeProvider rp = new RhymeProvider("WordBank.txt");
+			RhymeProvider rp = new RhymeProvider("../webapps/ROOT/RhymeBank/WordBank.txt");
 			rp.initialize();
 			list = rp.findRhyme(word);
 		}
-		resp.setContentType("text/plain");
-		resp.setCharacterEncoding("UTF-8");
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("UTF-8");
 		for(int i = 0; i < list.size(); i++) {
-			resp.getWriter().println(list.get(i) + "<br>");
+			response.getWriter().println(list.get(i) + "<br>");
 		}
 	}
+
 }
