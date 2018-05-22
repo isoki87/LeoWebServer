6bb0f71bf97e5ce23112bc46b997313e0e9f4e1b
isoki87 <helloleolin@gmail.com>
2018-05-22 11:46:55 -0700

    Switch from embedded jetty to tomcat web container

--- SudokuApiServlet.java
+++ SudokuApiServlet.java
@@ -1,6 +1,8 @@
-import java.io.IOException;
+package com.leo.servlet;
 
+import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,20 +10,22 @@ import javax.servlet.http.HttpServletResponse;
 import com.leo.sudoku.SudokuBoard;
 import com.leo.sudoku.SudokuGenerator;
 
-
-@SuppressWarnings("serial")
+/**
+ * Servlet implementation class Sudoku
+ */
+@WebServlet("/sudokuapi")
 public class SudokuApiServlet extends HttpServlet {
-	
-	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+	private static final long serialVersionUID = 1L;
+ 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		SudokuBoard board = new SudokuBoard();
 		SudokuGenerator sudGen = new SudokuGenerator();
 		sudGen.populateBoard(board);
-		resp.setContentType("text/plain");
-		resp.setCharacterEncoding("UTF-8");
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("UTF-8");
 		for(int i = 0; i < 9; i++){
 			for(int j = 0; j < 9; j++){
-				resp.getWriter().print(board.check(i, j));
+				response.getWriter().print(board.check(i, j));
 			}
 		}
 	}
